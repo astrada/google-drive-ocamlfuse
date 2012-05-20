222a354bdf470e3e7c89b0e30885ecff050a00b1
Alessandro Strada <alessandro.strada@gmail.com>
2012-05-20 23:50:15 +0200

    Add lenses to State

--- src/state.ml
+++ src/state.ml
@@ -8,6 +8,35 @@ type t = {
   last_access_token : string;
 }
 
+let auth_request_id = {
+  GapiLens.get = (fun x -> x.auth_request_id);
+  GapiLens.set = (fun v x -> { x with auth_request_id = v })
+}
+let auth_request_date = {
+  GapiLens.get = (fun x -> x.auth_request_date);
+  GapiLens.set = (fun v x -> { x with auth_request_date = v })
+}
+let user_id = {
+  GapiLens.get = (fun x -> x.user_id);
+  GapiLens.set = (fun v x -> { x with user_id = v })
+}
+let refresh_token = {
+  GapiLens.get = (fun x -> x.refresh_token);
+  GapiLens.set = (fun v x -> { x with refresh_token = v })
+}
+let last_access_token = {
+  GapiLens.get = (fun x -> x.last_access_token);
+  GapiLens.set = (fun v x -> { x with last_access_token = v })
+}
+
+let empty = {
+  auth_request_id = "";
+  auth_request_date = GapiDate.epoch;
+  user_id = "";
+  refresh_token = "";
+  last_access_token = "";
+}
+
 let of_table table =
   let get = Hashtbl.find table in
     { auth_request_id = get "auth_request_id";
@@ -18,10 +47,12 @@ let of_table table =
     }
 
 let to_table data =
+  let table = Hashtbl.create 8 in
   let add = Hashtbl.add table in
     add "auth_request_id" data.auth_request_id;
     GapiDate.to_string data.auth_request_date |> add "auth_request_date";
     add "user_id" data.user_id;
     add "refresh_token" data.refresh_token;
-    add "last_access_token" data.last_access_token
+    add "last_access_token" data.last_access_token;
+    table
 
