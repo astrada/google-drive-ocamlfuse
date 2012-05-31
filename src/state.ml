4b5e960c59ce57bb536731a0b88ff3ccbb1b4057
Alessandro Strada <alessandro.strada@gmail.com>
2012-06-01 00:06:58 +0200

    Remove user_id and add access_token_date

--- src/state.ml
+++ src/state.ml
@@ -3,9 +3,9 @@ open Utils.Infix
 type t = {
   auth_request_id : string;
   auth_request_date : GapiDate.t;
-  user_id : string;
   refresh_token : string;
   last_access_token : string;
+  access_token_date : GapiDate.t;
 }
 
 let auth_request_id = {
@@ -16,10 +16,6 @@ let auth_request_date = {
   GapiLens.get = (fun x -> x.auth_request_date);
   GapiLens.set = (fun v x -> { x with auth_request_date = v })
 }
-let user_id = {
-  GapiLens.get = (fun x -> x.user_id);
-  GapiLens.set = (fun v x -> { x with user_id = v })
-}
 let refresh_token = {
   GapiLens.get = (fun x -> x.refresh_token);
   GapiLens.set = (fun v x -> { x with refresh_token = v })
@@ -28,22 +24,26 @@ let last_access_token = {
   GapiLens.get = (fun x -> x.last_access_token);
   GapiLens.set = (fun v x -> { x with last_access_token = v })
 }
+let access_token_date = {
+  GapiLens.get = (fun x -> x.access_token_date);
+  GapiLens.set = (fun v x -> { x with access_token_date = v })
+}
 
 let empty = {
   auth_request_id = "";
   auth_request_date = GapiDate.epoch;
-  user_id = "";
   refresh_token = "";
   last_access_token = "";
+  access_token_date = GapiDate.epoch;
 }
 
 let of_table table =
   let get = Hashtbl.find table in
     { auth_request_id = get "auth_request_id";
       auth_request_date = get "auth_request_date" |> GapiDate.of_string;
-      user_id = get "user_id";
       refresh_token = get "refresh_token";
       last_access_token = get "last_access_token";
+      access_token_date = get "access_token_date" |> GapiDate.of_string;
     }
 
 let to_table data =
@@ -51,8 +51,8 @@ let to_table data =
   let add = Hashtbl.add table in
     add "auth_request_id" data.auth_request_id;
     GapiDate.to_string data.auth_request_date |> add "auth_request_date";
-    add "user_id" data.user_id;
     add "refresh_token" data.refresh_token;
     add "last_access_token" data.last_access_token;
+    GapiDate.to_string data.access_token_date |> add "access_token_date";
     table
 
