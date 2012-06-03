5829aca1835526043123f33a105f68b25d2360f8
Alessandro Strada <alessandro.strada@gmail.com>
2012-06-03 14:39:18 +0200

    Add helper functions
    * Add support functions to save/load state and config
    * Add exception logging

--- src/utils.ml
+++ src/utils.ml
@@ -1,8 +1,4 @@
-module Infix =
-struct
-  let (|>) x f = f x
-
-end
+open GapiUtils.Infix
 
 let verbose = ref false
 
@@ -12,4 +8,20 @@ let log_message format =
     Printf.printf format
   else
     Printf.ifprintf stdout format
+
+let log_exception e =
+  let message = Printexc.to_string e in
+  let backtrace = Printexc.get_backtrace () in
+    log_message "Exception:%s\n" message;
+    log_message "Backtrace:%s\n%!" backtrace
+
+(* Hashtbl *)
+let safe_find table key =
+  try
+    let v = Hashtbl.find table key in
+      Some v
+  with Not_found -> None
+
+let get_from_string_table table (key : string) conv default =
+  safe_find table key |> Option.map_default conv default
 
