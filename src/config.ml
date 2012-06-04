3e4f84d4028bc07865d82416bb01e2bb4bfbc698
Alessandro Strada <alessandro.strada@gmail.com>
2012-06-04 22:36:02 +0200

    Add read-only, umask, sqlite3 busy timeout config options

--- src/config.ml
+++ src/config.ml
@@ -8,6 +8,12 @@ type t = {
   debug : bool;
   (* Number of seconds metadata should be cached. *)
   metadata_cache_time : int;
+  (* Specifies whether local filesystem is mounted read-only. *)
+  read_only : bool;
+  (* umask mount option *)
+  umask : int;
+  (* Sqlite3 busy handler timeout in milliseconds *)
+  sqlite3_busy_timeout : int;
 }
 
 let debug = {
@@ -18,17 +24,49 @@ let metadata_cache_time = {
   GapiLens.get = (fun x -> x.metadata_cache_time);
   GapiLens.set = (fun v x -> { x with metadata_cache_time = v })
 }
+let read_only = {
+  GapiLens.get = (fun x -> x.read_only);
+  GapiLens.set = (fun v x -> { x with read_only = v })
+}
+let umask = {
+  GapiLens.get = (fun x -> x.umask);
+  GapiLens.set = (fun v x -> { x with umask = v })
+}
+let sqlite3_busy_timeout = {
+  GapiLens.get = (fun x -> x.sqlite3_busy_timeout);
+  GapiLens.set = (fun v x -> { x with sqlite3_busy_timeout = v })
+}
 
-let empty = {
+let umask =
+  let prev_umask = Unix.umask 0 in
+  let _ = Unix.umask prev_umask in
+    prev_umask
+
+let default = {
   debug = false;
-  metadata_cache_time = 0;
+  metadata_cache_time = 60;
+  read_only = true;
+  umask;
+  sqlite3_busy_timeout = 500;
+}
+
+let default_debug = {
+  debug = true;
+  metadata_cache_time = 60;
+  read_only = true;
+  umask;
+  sqlite3_busy_timeout = 500;
 }
 
 let of_table table =
   let get k = Utils.get_from_string_table table k in
-    { debug = get "debug" bool_of_string empty.debug;
+    { debug = get "debug" bool_of_string default.debug;
       metadata_cache_time =
-        get "metadata_cache_time" int_of_string empty.metadata_cache_time;
+        get "metadata_cache_time" int_of_string default.metadata_cache_time;
+      read_only = get "read_only" bool_of_string default.read_only;
+      umask = get "umask" int_of_string default.umask;
+      sqlite3_busy_timeout =
+        get "sqlite3_busy_timeout" int_of_string default.sqlite3_busy_timeout;
     }
 
 let to_table data =
@@ -36,17 +74,10 @@ let to_table data =
   let add = Hashtbl.add table in
     add "debug" (data.debug |> string_of_bool);
     add "metadata_cache_time" (data.metadata_cache_time |> string_of_int);
+    add "read_only" (data.read_only |> string_of_bool);
+    add "umask" (data.umask |> string_of_int);
+    add "sqlite3_busy_timeout" (data.sqlite3_busy_timeout |> string_of_int);
     table
-
-let default = {
-  debug = false;
-  metadata_cache_time = 60;
-}
-
-let default_debug = {
-  debug = true;
-  metadata_cache_time = 60;
-}
 
 let debug_print out_ch start_time curl info_type info =
   let time = Unix.gettimeofday () in
