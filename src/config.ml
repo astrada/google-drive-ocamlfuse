3b6710bcbb425e72c76191986e66d753df84fda7
Alessandro Strada <alessandro.strada@gmail.com>
2015-02-06 00:16:01 +0100

    Update to version 0.5.14

--- src/config.ml
+++ src/config.ml
@@ -2,7 +2,7 @@ open GapiUtils.Infix
 open GapiLens.Infix
 
 let application_name = "google-drive-ocamlfuse"
-let version = "0.5.13"
+let version = "0.5.14"
 
 module ConflictResolutionStrategy =
 struct
