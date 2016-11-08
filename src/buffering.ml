b182632325e965e5279e4a1156e6c098911b1020
Alessandro Strada <alessandro.strada@gmail.com>
2016-11-09 00:29:37 +0100

    Start implementing memory buffers

--- src/buffering.ml
+++ src/buffering.ml
@@ -1,10 +1,15 @@
-module Buffer = struct
+exception Buffer_underrun
 
+module Block =
+struct
   type range = {
     start_pos : int64;
     end_pos : int64;
     buf_offset : int;
   }
+
+  let get_range_length range =
+    Int64.sub range.end_pos range.start_pos |> Int64.to_int
 
   let check_range offset range =
     offset >= range.start_pos && offset < range.end_pos
@@ -15,40 +20,52 @@ module Buffer = struct
     if List.length ranges > 1 then
       raise (Invalid_argument "get_arr: overlapping ranges")
     else if List.length ranges = 0 then
-      raise (Invalid_argument "get_arr: range not found");
+      raise Not_found;
     List.hd ranges
 
+  type state =
+      Empty
+    | Clean
+    | Dirty
+
   type t = {
-    arr : (char,
-           Bigarray.int8_unsigned_elt,
-           Bigarray.c_layout) Bigarray.Array1.t;
+    buffer : (char,
+              Bigarray.int8_unsigned_elt,
+              Bigarray.c_layout) Bigarray.Array1.t;
     ranges : range list;
+    state : state;
+    last_update : float;
+    mutex : Mutex.t;
   }
 
   let create size = {
-    arr = Bigarray.Array1.create Bigarray.char Bigarray.c_layout size;
+    buffer = Bigarray.Array1.create Bigarray.char Bigarray.c_layout size;
     ranges = [];
+    state = Empty;
+    last_update = Unix.gettimeofday ();
+    mutex = Mutex.create ();
   }
 
-  let is_in_range offset buffer =
-    List.exists (check_range offset) buffer.ranges
+  let is_in_range offset block =
+    List.exists (check_range offset) block.ranges
 
-  let blit_to_arr dest_arr offset buffer =
+  let blit_to_arr dest_arr offset block =
+    if block.state = Empty then raise Buffer_underrun;
     let rec loop off dest_sub_arr =
       let dest_len = Bigarray.Array1.dim dest_sub_arr in
-      let range = get_range off buffer.ranges in
+      let range = get_range off block.ranges in
       if Int64.to_int (Int64.sub range.end_pos off) >= dest_len then begin
         let buf_off =
           Int64.to_int (Int64.sub off range.start_pos) + range.buf_offset in
         let src_arr =
-          Bigarray.Array1.sub buffer.arr buf_off dest_len in
+          Bigarray.Array1.sub block.buffer buf_off dest_len in
         Bigarray.Array1.blit src_arr dest_sub_arr
       end else begin
         let src_len = Int64.sub range.end_pos off |> Int64.to_int in
         let buf_off =
           Int64.to_int (Int64.sub off range.start_pos) + range.buf_offset in
         let src_arr =
-          Bigarray.Array1.sub buffer.arr buf_off src_len in
+          Bigarray.Array1.sub block.buffer buf_off src_len in
         let dest_left_sub_arr =
           Bigarray.Array1.sub dest_sub_arr 0 src_len in
         Bigarray.Array1.blit src_arr dest_left_sub_arr;
@@ -57,7 +74,146 @@ module Buffer = struct
         loop range.end_pos dest_right_sub_arr
       end
     in
-    loop offset dest_arr
+    Utils.with_lock block.mutex
+      (fun () -> loop offset dest_arr)
+
+  let write src_arr offset block =
+    let len = Bigarray.Array1.dim src_arr in
+    let buf_offset =
+      match block.ranges with
+      | [] -> 0
+      | last :: _ -> last.buf_offset + (get_range_length last) in
+    let range = {
+      start_pos = offset;
+      end_pos = Int64.add offset (Int64.of_int len);
+      buf_offset;
+    } in
+    let dest_arr = Bigarray.Array1.sub block.buffer range.buf_offset len in
+    Utils.with_lock block.mutex
+      (fun () -> Bigarray.Array1.blit src_arr dest_arr);
+    { block with
+      ranges = range :: block.ranges;
+      state = Dirty;
+      last_update = Unix.gettimeofday ();
+    }
+
+  let fill src_arr start_pos block =
+    let len = Bigarray.Array1.dim src_arr in
+    let range = {
+      start_pos;
+      end_pos = Int64.add start_pos (Int64.of_int len);
+      buf_offset = 0;
+    } in
+    Utils.with_lock block.mutex
+      (fun () -> Bigarray.Array1.blit src_arr block.buffer);
+    { block with
+      ranges = [range];
+      state = Clean;
+      last_update = Unix.gettimeofday ();
+    }
+
+end
+
+module FileBlocks =
+struct
+  type t = {
+    blocks : (int, Block.t) Hashtbl.t;
+    block_size : int;
+    mutex : Mutex.t;
+  }
+
+  let create ?(n = 16) block_size = {
+    blocks = Hashtbl.create n;
+    block_size;
+    mutex = Mutex.create ();
+  }
+
+  let get_block_index start_pos file_blocks =
+    Int64.div start_pos
+      (Int64.of_int file_blocks.block_size) |> Int64.to_int
+
+  let get_block block_index file_blocks =
+    Utils.with_lock file_blocks.mutex
+      (fun () ->
+         match Utils.safe_find file_blocks.blocks block_index with
+         | None ->
+           let b = Block.create file_blocks.block_size in
+           Hashtbl.add file_blocks.blocks block_index b;
+           b
+         | Some b -> b
+      )
+
+  let replace_block block_index block file_blocks =
+    Utils.with_lock file_blocks.mutex
+      (fun () ->
+         Hashtbl.replace file_blocks.blocks block_index block
+      )
+
+  let write src_array start_pos file_blocks =
+    let block_index = get_block_index start_pos file_blocks in
+    let block = get_block block_index file_blocks in
+    let block = Block.write src_array start_pos block in
+    replace_block block_index block file_blocks;
+    file_blocks
+
+  let fill_block src_array start_pos file_blocks =
+    let block_index = get_block_index start_pos file_blocks in
+    let block = get_block block_index file_blocks in
+    let block = Block.fill src_array start_pos block in
+    replace_block block_index block file_blocks;
+    file_blocks
+
+  let blit_to_arr dest_arr start_pos file_blocks =
+    let block_index = get_block_index start_pos file_blocks in
+    let block = get_block block_index file_blocks in
+    Block.blit_to_arr dest_arr start_pos block
+
+end
+
+module Buffers =
+struct
+  type t = {
+    table : (string, FileBlocks.t) Hashtbl.t;
+    mutex : Mutex.t;
+  }
+
+  let create ?(n = 16) () = {
+    table = Hashtbl.create n;
+    mutex = Mutex.create ();
+  }
+
+  let add path block_size buffers =
+    Utils.with_lock buffers.mutex
+      (fun () ->
+         let file_blocks = FileBlocks.create block_size in
+         Hashtbl.add buffers.table path file_blocks;
+         buffers
+      )
+
+  let write_to_buffer path src_arr offset buffers =
+    let file_blocks =
+      Utils.with_lock buffers.mutex
+        (fun () -> Hashtbl.find buffers.table path) in
+    FileBlocks.write src_arr offset file_blocks |> ignore;
+    buffers
+
+  let fill_block path src_array start_pos buffers =
+    let file_blocks =
+      Utils.with_lock buffers.mutex
+        (fun () -> Hashtbl.find buffers.table path) in
+    FileBlocks.fill_block src_array start_pos file_blocks |> ignore;
+    buffers
+
+  let blit_buffer_to_arr path dest_arr offset buffers =
+    let file_blocks =
+      Utils.with_lock buffers.mutex
+        (fun () -> Hashtbl.find buffers.table path) in
+    FileBlocks.blit_to_arr dest_arr offset file_blocks
+    
+  let remove_buffer path buffers =
+    Utils.with_lock buffers.mutex
+      (fun () -> Hashtbl.remove buffers.table path);
+    buffers
 
 end
 
--- src/testBuffering.ml
+++ src/testBuffering.ml
@@ -8,24 +8,10 @@ let print_array arr =
   done;
   r
 
-let create_buffer () =
-  let b = Buffering.Buffer.create 15 in
-  let b = {
-    b with Buffering.Buffer.ranges = [
-      { Buffering.Buffer.start_pos = 0L;
-        end_pos = 5L;
-        buf_offset = 0
-      };
-      { Buffering.Buffer.start_pos = 5L;
-        end_pos = 10L;
-        buf_offset = 5
-      };
-      { Buffering.Buffer.start_pos = 10L;
-        end_pos = 15L;
-        buf_offset = 10
-      }]} in
-  Bigarray.Array1.fill b.Buffering.Buffer.arr 'a';
-  b
+let create_source () =
+  let src = Bigarray.Array1.create Bigarray.char Bigarray.c_layout 15 in
+  Bigarray.Array1.fill src 'a';
+  src
 
 let create_destination () =
   let dest = Bigarray.Array1.create Bigarray.char Bigarray.c_layout 3 in
@@ -38,40 +24,109 @@ let create_result () =
   Bigarray.Array1.set dest 1 'b';
   dest
 
+let create_block () =
+  let b = Buffering.Block.create 15 in
+  let b = Buffering.Block.fill (create_source ()) 3L b in
+  { b with Buffering.Block.ranges = [
+          { Buffering.Block.start_pos = 3L;
+            end_pos = 8L;
+            buf_offset = 0
+          };
+          { Buffering.Block.start_pos = 8L;
+            end_pos = 13L;
+            buf_offset = 5
+          };
+          { Buffering.Block.start_pos = 13L;
+            end_pos = 18L;
+            buf_offset = 10
+          }]}
+
 let test_blit_to_arr_first_range () =
-  let b = create_buffer () in
-  Bigarray.Array1.set b.Buffering.Buffer.arr 1 'b';
+  let b = create_block () in
+  Bigarray.Array1.set b.Buffering.Block.buffer 1 'b';
   let dest = create_destination () in
-  Buffering.Buffer.blit_to_arr dest 0L b;
+  Buffering.Block.blit_to_arr dest 3L b;
   assert_equal
     ~printer:print_array
     (create_result ())
     dest
 
 let test_blit_to_arr_second_range () =
-  let b = create_buffer () in
-  Bigarray.Array1.set b.Buffering.Buffer.arr 7 'b';
+  let b = create_block () in
+  Bigarray.Array1.set b.Buffering.Block.buffer 7 'b';
   let dest = create_destination () in
-  Buffering.Buffer.blit_to_arr dest 6L b;
+  Buffering.Block.blit_to_arr dest 9L b;
   assert_equal
     ~printer:print_array
     (create_result ())
     dest
 
 let test_blit_to_arr_second_and_third_range () =
-  let b = create_buffer () in
-  Bigarray.Array1.set b.Buffering.Buffer.arr 10 'b';
+  let b = create_block () in
+  Bigarray.Array1.set b.Buffering.Block.buffer 10 'b';
   let dest = create_destination () in
-  Buffering.Buffer.blit_to_arr dest 9L b;
+  Buffering.Block.blit_to_arr dest 12L b;
   assert_equal
     ~printer:print_array
     (create_result ())
     dest
+
+let test_buffering () =
+  (*Utils.verbose := true;*)
+  let block_size = 25 in
+  let stream_buffer_size = 5 in
+  let n = block_size / stream_buffer_size in
+  let path = "test" in
+  let buffers = Buffering.Buffers.create () in
+  let download () =
+    Buffering.Buffers.add path block_size buffers |> ignore;
+    let arr =
+      Bigarray.Array1.create Bigarray.char Bigarray.c_layout block_size in
+    for i = 0 to (n - 1) do
+      let sub_arr =
+        Bigarray.Array1.sub arr (i * stream_buffer_size) stream_buffer_size in
+      Bigarray.Array1.fill sub_arr (Char.chr (Char.code '0' + i));
+    done;
+    Thread.delay 0.5;
+    Buffering.Buffers.fill_block path arr 0L buffers |> ignore;
+  in
+  let create_arr _ =
+    Bigarray.Array1.create
+      Bigarray.char Bigarray.c_layout stream_buffer_size in
+  let dest_arrs = Array.init n create_arr in
+  let stream i =
+    let offset = Int64.of_int (i * stream_buffer_size) in
+    Utils.with_retry
+      ~filter_exception:(function
+          | Buffering.Buffer_underrun -> true
+          | Not_found -> download (); true
+          | _ -> false)
+      (fun () ->
+         Buffering.Buffers.blit_buffer_to_arr
+           path dest_arrs.(i) offset buffers |> ignore)
+      "stream"
+  in
+  let tq = Queue.create () in
+  for i = 0 to n - 1 do
+     Queue.add (Thread.create stream i) tq;
+  done;
+  Queue.iter Thread.join tq;
+  for i = 0 to n - 1 do
+    let result =
+      Bigarray.Array1.create
+        Bigarray.char Bigarray.c_layout stream_buffer_size in
+    Bigarray.Array1.fill result (Char.chr (Char.code '0' + i));
+    assert_equal
+      ~printer:print_array
+      result
+      dest_arrs.(i);
+  done
 
 let suite = "Buffering test" >:::
   ["test_blit_to_arr_first_range" >:: test_blit_to_arr_first_range;
    "test_blit_to_arr_second_range" >:: test_blit_to_arr_second_range;
    "test_blit_to_arr_second_and_third_range" >::
      test_blit_to_arr_second_and_third_range;
+   "test_buffering" >:: test_buffering
   ]
 
