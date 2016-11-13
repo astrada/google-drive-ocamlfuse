5af1aa09cd6e4e0f564f45e5288fbed7aebf84bb
Alessandro Strada <alessandro.strada@gmail.com>
2016-11-13 19:53:15 +0100

    Add test

--- src/testBuffering.ml
+++ src/testBuffering.ml
@@ -3,6 +3,22 @@ open OUnit
 open GapiMonad
 open GapiMonad.SessionM.Infix
 
+let print_array arr =
+  let len = Bigarray.Array1.dim arr in
+  let r = String.make len ' ' in
+  for i = 0 to (len - 1) do
+    r.[i] <- Bigarray.Array1.get arr i
+  done;
+  r
+
+let session = {
+  GapiConversation.Session.curl = GapiCurl.Initialized;
+  config = GapiConfig.default;
+  auth = GapiConversation.Session.NoAuth;
+  cookies = [];
+  etag = "";
+}
+
 let test_with_lock_m () =
   let counter1 = ref 0 in
   let counter2 = ref 0 in
@@ -19,13 +35,6 @@ let test_with_lock_m () =
        SessionM.return ()
       )
   in
-  let session = {
-    GapiConversation.Session.curl = GapiCurl.Initialized;
-    config = GapiConfig.default;
-    auth = GapiConversation.Session.NoAuth;
-    cookies = [];
-    etag = "";
-  } in
   let tq = Queue.create () in
   for i = 1 to 10 do
     let t = Thread.create switch session in
@@ -45,7 +54,61 @@ let test_with_lock_m () =
     9
     !counter2
 
+let test_get_full_block_and_blit () =
+  let remote_id = "test" in
+  let resource_size = 24L in
+  let block_size = 16 in
+  let stream_block_size = 8 in
+
+  let fill_array offset arr =
+    begin if offset = 0L then begin
+        Bigarray.Array1.fill arr 'a';
+        Bigarray.Array1.set arr stream_block_size 'c';
+      end else
+        Bigarray.Array1.fill arr 'b'
+    end;
+    SessionM.return ()
+  in
+
+  let memory_buffers = Buffering.MemoryBuffers.create block_size in
+  let destination =
+    Bigarray.Array1.create
+      Bigarray.char Bigarray.c_layout (Int64.to_int resource_size) in
+  let init_subs i =
+    Bigarray.Array1.sub
+      destination (i * stream_block_size) stream_block_size in
+  let dest_arrs = Array.init 3 init_subs in
+  let stream buffer offset =
+    Buffering.MemoryBuffers.get_full_block_and_blit
+      remote_id offset resource_size
+      (fun start_pos block_buffer -> fill_array start_pos block_buffer)
+      buffer memory_buffers
+  in
+  for i = 0 to ((Int64.to_int resource_size) / stream_block_size - 1) do
+    let offset = Int64.of_int (i * stream_block_size) in
+    stream dest_arrs.(i) offset session |> ignore;
+    let result =
+      let arr =
+        Bigarray.Array1.create
+          Bigarray.char Bigarray.c_layout stream_block_size in
+      begin if i = 2 then
+          Bigarray.Array1.fill arr 'b'
+        else begin
+          Bigarray.Array1.fill arr 'a';
+          if i = 1 then
+            Bigarray.Array1.set arr 0 'c';
+        end
+      end;
+      arr
+    in
+    assert_equal
+      ~printer:print_array
+      result
+      dest_arrs.(i);
+  done
+
 let suite = "Buffering test" >:::
             ["test_with_lock_m" >:: test_with_lock_m;
+             "test_get_full_block_and_blit" >:: test_get_full_block_and_blit;
             ]
 
