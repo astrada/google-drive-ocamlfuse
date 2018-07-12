294a66c11ab09f6da2ac7827769714b2117e571d
Alessandro Strada <alessandro.strada@gmail.com>
2018-07-12 22:17:30 +0200

    Update version to 0.6.26

--- src/config.ml
+++ src/config.ml
@@ -2,7 +2,7 @@ open GapiUtils.Infix
 open GapiLens.Infix
 
 let application_name = "google-drive-ocamlfuse"
-let version = "0.6.25"
+let version = "0.6.26"
 
 type t = {
   (* Number of seconds metadata should be cached. *)
