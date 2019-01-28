fe1d48172b03dd5abf8b1aa2bfad127dbd55a4fd
Alessandro Strada <alessandro.strada@gmail.com>
2019-01-28 20:24:45 +0100

    Update copyright years

--- bin/gdfuse.ml
+++ bin/gdfuse.ml
@@ -652,7 +652,7 @@ let () =
 
   if !show_version then begin
     Printf.printf "google-drive-ocamlfuse, version %s\n\
-                   Copyright (C) 2012-2018 Alessandro Strada\n\
+                   Copyright (C) 2012-2019 Alessandro Strada\n\
                    License MIT\n"
       Config.version;
   end else begin
